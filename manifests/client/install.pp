44003a48eccbef6294bd82aa54ecc57fd88c3a69
Dmitry Sergeev <identw@gmail.com>
2019-01-10 13:13:40 +0500

    Install mysql-client via ensure_packages. In order to avoid duplicate resource errors, when trying to install a package in external code.

--- manifests/client/install.pp
+++ manifests/client/install.pp
@@ -6,12 +6,16 @@
 class mysql::client::install {
 
   if $mysql::client::package_manage {
-
-    package { 'mysql_client':
+    ensure_packages('mysql_client', {
       ensure          => $mysql::client::package_ensure,
       install_options => $mysql::client::install_options,
       name            => $mysql::client::package_name,
-    }
+    })
+    # package { 'mysql_client':
+    #   ensure          => $mysql::client::package_ensure,
+    #   install_options => $mysql::client::install_options,
+    #   name            => $mysql::client::package_name,
+    # }
 
   }
 
